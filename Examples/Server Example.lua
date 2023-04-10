25c607ef04f8a6dc070b6128b2b6859fa2c145b2
zblox164 <68709543+zblox164@users.noreply.github.com>
2023-04-10 06:53:12 -0500

    Update Server Example.lua

--- Examples/Server Example.lua
+++ Examples/Server Example.lua
@@ -2,69 +2,66 @@ local replicatedStorage = game:GetService("ReplicatedStorage")
 
 -- Ignore the top three functions
 local function checkHitbox(character, object)
-	if object then
-		local collided = false	
-		local collisionPoints = workspace:GetPartsInPart(object.PrimaryPart)
+	if not object then return false end	
+	local collisionPoints = workspace:GetPartsInPart(object.PrimaryPart)
 
-		for i = 1, #collisionPoints, 1 do
-			if collisionPoints[i].CanTouch and not collisionPoints[i]:IsDescendantOf(object) and not collisionPoints[i]:IsDescendantOf(character) then
-				collided = true
+	-- Checks if there is collision on any object that is not a child of the object and is not a child of the player
+	for i = 1, #collisionPoints, 1 do
+		if not collisionPoints[i].CanTouch then continue end
+		if not (not collisionPoints[i]:IsDescendantOf(object) and not collisionPoints[i]:IsDescendantOf(character)) then continue end
 
-				break
-			end
-		end
-
-		return collided
+		return true
 	end
+
+	return false
 end
 
-local function checkBoundaries(plt: BasePart, primary: BasePart): boolean
-	local pos = plt.CFrame
-	local size = CFrame.fromOrientation(0, primary.Orientation.Y, 0)*primary.Size
-	local currentPos = pos:Inverse()*primary.CFrame
+-- Checks if the object exceeds the boundries given by the plot
+local function checkBoundaries(plot: BasePart, primary: BasePart): boolean
+	local pos: CFrame = plot.CFrame
+	local size: Vector3 = CFrame.fromOrientation(0, primary.Orientation.Y*math.pi/180, 0)*primary.Size
+	local currentPos: CFrame = pos:Inverse()*primary.CFrame
 
-	local xBound = (plt.Size.X - size.X)
-	local zBound = (plt.Size.Z - size.Z)
+	local xBound: number = (plot.Size.X - size.X)
+	local zBound: number = (plot.Size.Z - size.Z)
 
 	return currentPos.X > xBound or currentPos.X < -xBound or currentPos.Z > zBound or currentPos.Z < -zBound
 end
 
-local function handleCollisions(char, item, c)
-	if c then
-		if not checkHitbox(char, item) then
-			item.PrimaryPart.Transparency = 1
-
-			return true
-		else
-			item:Destroy()
-
-			return false
-		end
-	else
-		item.PrimaryPart.Transparency = 1
-
-		return true
-	end
+local function handleCollisions(character: Model, item, collisions: boolean): boolean
+	if not collisions then item.PrimaryPart.Transparency = 1; return true end
+	
+	local collision = checkHitbox(character, item)
+	if collision then item:Destroy(); return false end
+	
+	item.PrimaryPart.Transparency = 1
+	return true
 end
 
 --Ignore above
 
-local function place(plr, name, location, prefabs, cframe, c, plot)
+-- Edit if you want to have a server check if collisions are enabled or disabled
+local function getCollisions(name: string): boolean
+	return true
+end
+
+local function place(player, name: string, location: Instance, prefabs: Instance, cframe: CFrame, plot: BasePart)
+	local collisions = getCollisions(name)
 	local item = prefabs:FindFirstChild(name):Clone()
 	item.PrimaryPart.CanCollide = false
 	item:PivotTo(cframe)
 	
 	if plot then
-		if checkBoundaries(plot, item.PrimaryPart) then
-			return
+		if checkBoundaries(plot, item.PrimaryPart) then 
+			return 
 		end
 
 		item.Parent = location
 
-		return handleCollisions(plr.Character, item, c)
-	else
-		return handleCollisions(plr.Character, item, c)
+		return handleCollisions(player.Character, item, collisions)
 	end
+	
+	return handleCollisions(player.Character, item, collisions)
 end
 
 replicatedStorage.remotes.functions.requestPlacement.OnServerInvoke = place
