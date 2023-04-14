6462bd3ee427d1e8ec88b1b0baa736c6773593cc
zblox164 <68709543+zblox164@users.noreply.github.com>
2023-04-13 23:17:38 -0500

    Update Server Example.lua

--- Examples/Server Example.lua
+++ Examples/Server Example.lua
@@ -1,14 +1,14 @@
 local replicatedStorage = game:GetService("ReplicatedStorage")
 
 -- Ignore the top three functions
-local function checkHitbox(character, object)
+local function checkHitbox(character, object, plot)
 	if not object then return false end	
 	local collisionPoints = workspace:GetPartsInPart(object.PrimaryPart)
 
 	-- Checks if there is collision on any object that is not a child of the object and is not a child of the player
 	for i = 1, #collisionPoints, 1 do
 		if not collisionPoints[i].CanTouch then continue end
-		if not (not collisionPoints[i]:IsDescendantOf(object) and not collisionPoints[i]:IsDescendantOf(character)) then continue end
+		if not (not collisionPoints[i]:IsDescendantOf(object) and not collisionPoints[i]:IsDescendantOf(character)) and not collisionPoints[i]:IsDescendantOf() then continue end
 
 		return true
 	end
@@ -28,10 +28,10 @@ local function checkBoundaries(plot: BasePart, primary: BasePart): boolean
 	return currentPos.X > xBound or currentPos.X < -xBound or currentPos.Z > zBound or currentPos.Z < -zBound
 end
 
-local function handleCollisions(character: Model, item, collisions: boolean): boolean
+local function handleCollisions(character: Model, item, collisions: boolean, plot): boolean
 	if not collisions then item.PrimaryPart.Transparency = 1; return true end
 	
-	local collision = checkHitbox(character, item)
+	local collision = checkHitbox(character, item, plot)
 	if collision then item:Destroy(); return false end
 	
 	item.PrimaryPart.Transparency = 1
@@ -58,10 +58,10 @@ local function place(player, name: string, location: Instance, prefabs: Instance
 
 		item.Parent = location
 
-		return handleCollisions(player.Character, item, collisions)
+		return handleCollisions(player.Character, item, collisions, plot)
 	end
 	
-	return handleCollisions(player.Character, item, collisions)
+	return handleCollisions(player.Character, item, collisions, plot)
 end
 
 replicatedStorage.remotes.functions.requestPlacement.OnServerInvoke = place
